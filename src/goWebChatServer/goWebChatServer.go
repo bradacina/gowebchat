24c6ed02cc86f1fb815dce98738910679b2b4d81
Bogdan Radacina <Bogdan Radacina@localhost>
2013-05-30 22:16:38 -0600

    Detecting client names

--- src/goWebChatServer/goWebChatServer.go
+++ src/goWebChatServer/goWebChatServer.go
@@ -2,16 +2,49 @@ package main
 
 import (
 	"io"
+	"log"
 	"net/http"
 
 	"code.google.com/p/go.net/websocket"
 )
 
+type Client struct {
+	Name string
+	Con  *websocket.Conn
+}
+
+var clients []Client
+
 func ChatHandler(ws *websocket.Conn) {
+
+	req := ws.Request()
+
+	err := req.ParseForm()
+	if err != nil {
+		ws.Close()
+		log.Printf("Can't parse form")
+	}
+
+	name, ok := req.Form["name"]
+
+	if !ok {
+		log.Printf("Client did not provide a name")
+		ws.Close()
+	}
+
+	client := Client{Name: name[0], Con: ws}
+
+	clients = append(clients, client)
+
+	log.Print(len(clients))
+
 	io.Copy(ws, ws)
 }
 
 func main() {
+
+	clients = make([]Client, 0)
+
 	http.Handle("/", http.FileServer(http.Dir("../../html")))
 	http.Handle("/chat", websocket.Handler(ChatHandler))
 	err := http.ListenAndServe(":12345", nil)
