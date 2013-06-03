be74d0e4d22d1240da7ce25ad34b8748d2ea9e6f
Bogdan Radacina <bradacina@users.noreply.github.com>
2013-06-03 13:51:44 -0600

    Separating Client in its own struct

---
+++ src/goWebChat/client.go
@@ -0,0 +1,79 @@
+package goWebChat
+
+import (
+	"code.google.com/p/go.net/websocket"
+	"log"
+)
+
+type Client struct {
+	Name      string
+	ReadChan  chan []byte
+	WriteChan chan []byte
+	Closed    chan bool
+
+	con        *websocket.Conn
+	writeClose chan bool
+}
+
+func (c *Client) Close() error {
+	return c.con.Close()
+}
+
+func NewClient(name string, ws *websocket.Conn) Client {
+
+	client := Client{Name: name, con: ws}
+	client.ReadChan = make(chan []byte, 10)
+	client.WriteChan = make(chan []byte, 10)
+	client.Closed = make(chan bool, 0)
+	client.writeClose = make(chan bool, 0)
+
+	go client.readLoop()
+	go client.writeLoop()
+
+	return client
+}
+
+func (c *Client) logMessage(a ...interface{}) {
+	newMsg := make([]interface{}, 0)
+	newMsg = append(newMsg, c.Name+"->")
+	newMsg = append(newMsg, a...)
+	log.Println(newMsg...)
+}
+
+func (c *Client) readLoop() {
+
+	for {
+		bytes := make([]byte, 1024)
+		nBytes, err := c.con.Read(bytes)
+		if err != nil {
+			c.logMessage("Got error when reading from ", c.Name)
+			break
+		}
+
+		if nBytes == 0 {
+			c.logMessage("Read 0 bytes")
+		}
+
+		// send the data down the channel
+		copyBytes := make([]byte, nBytes)
+		copy(copyBytes, bytes[0:nBytes])
+		c.ReadChan <- copyBytes
+
+		c.logMessage("Read ", bytes[0:nBytes])
+	}
+	c.con.Close()
+	c.Closed <- true
+}
+
+func (c *Client) writeLoop() {
+	for {
+		select {
+		case writeBytes := <-c.WriteChan:
+			c.logMessage("On WriteChan: ", writeBytes)
+			c.con.Write(writeBytes)
+		case <-c.writeClose:
+			c.logMessage("Write Loop stopping due to client closed event")
+			return
+		}
+	}
+}
--- src/goWebChatServer/goWebChatServer.go
+++ src/goWebChatServer/goWebChatServer.go
@@ -1,19 +1,16 @@
 package main
 
 import (
-	"io"
+	"goWebChat"
 	"log"
 	"net/http"
 
 	"code.google.com/p/go.net/websocket"
 )
 
-type Client struct {
-	Name string
-	Con  *websocket.Conn
-}
-
-var clients []Client
+var clients []*goWebChat.Client
+var registerClient chan *goWebChat.Client
+var unregisterClient chan *goWebChat.Client
 
 func ChatHandler(ws *websocket.Conn) {
 
@@ -30,20 +27,74 @@ func ChatHandler(ws *websocket.Conn) {
 	if !ok {
 		log.Printf("Client did not provide a name")
 		ws.Close()
+		return
 	}
 
-	client := Client{Name: name[0], Con: ws}
+	client := goWebChat.NewClient(name[0], ws)
+	clientPtr := &client
 
-	clients = append(clients, client)
+	defer client.Close()
+	defer func() { unregisterClient <- clientPtr }()
 
-	log.Print(len(clients))
+	log.Println("Number of clients before: ", len(clients))
 
-	io.Copy(ws, ws)
+	registerClient <- clientPtr
+
+	// client process loop
+	for {
+		select {
+		case readBytes := <-client.ReadChan:
+			log.Println("On ReadChan: ", readBytes)
+
+			go broadcastToAllClients(append([]byte(client.Name+" said-> "), readBytes...))
+
+		case <-client.Closed:
+			log.Println("Connection on client ", client.Name, " was closed")
+			return
+		}
+	}
+}
+
+func broadcastToAllClients(msg []byte) {
+	for _, val := range clients {
+		val.WriteChan <- msg
+	}
+}
+
+func registrationLoop() {
+	for {
+		select {
+		case addClient := <-registerClient:
+			clients = append(clients, addClient)
+			log.Println("Adding client to array: ", addClient.Name)
+
+			broadcastToAllClients([]byte(addClient.Name + " has connected"))
+		case removeClient := <-unregisterClient:
+
+			log.Println("Removing client from array: ", removeClient.Name)
+			index := -1
+			for k, v := range clients {
+				if v == removeClient {
+					index = k
+					break
+				}
+			}
+
+			if index != -1 {
+				clients = append(clients[:index], clients[index+1:]...)
+				broadcastToAllClients([]byte(removeClient.Name + " has disconnected"))
+			}
+		}
+	}
 }
 
 func main() {
 
-	clients = make([]Client, 0)
+	clients = make([]*goWebChat.Client, 0)
+	registerClient = make(chan *goWebChat.Client, 10)
+	unregisterClient = make(chan *goWebChat.Client, 10)
+
+	go registrationLoop()
 
 	http.Handle("/", http.FileServer(http.Dir("../../html")))
 	http.Handle("/chat", websocket.Handler(ChatHandler))
