1708b98087a69a4a886066898c92f06ff215dee3
bogdan r <bogdan.radacina@gmail.com>
2013-06-05 15:54:07 -0600

    Server now sends the list of connected users to any new user conecting. Server sends messages to all clients when a client joints or departs.

--- src/goWebChatServer/goWebChatServer.go
+++ src/goWebChatServer/goWebChatServer.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"goWebChat"
 	"log"
 	"net/http"
@@ -28,10 +29,20 @@ func ChatHandler(ws *websocket.Conn) {
 		return
 	}
 
-	client := goWebChat.NewClient(name[0], ws)
+	uniqueName := make(chan string)
+	var newName string
+
+	clientsMap.GetUniqueName <- name[0]
+	select {
+	case newName = <-uniqueName:
+
+	}
+
+	client := goWebChat.NewClient(newname, ws)
 	clientPtr := &client
 
-	defer client.Close()
+	defer log.Println("Exiting handler function.")
+	//defer client.Close()
 	defer func() { clientsMap.UnregisterClient <- clientPtr }()
 
 	clientsMap.RegisterClient <- clientPtr
@@ -39,14 +50,67 @@ func ChatHandler(ws *websocket.Conn) {
 	// client process loop
 	for {
 		select {
-		case readBytes := <-client.ReadChan:
+		case <-clientPtr.Closed:
+			log.Println("Connection on client ", client.Name, " was closed")
+			return
+
+		case readBytes := <-clientPtr.ReadChan:
 			log.Println("On ReadChan: ", string(readBytes))
 
 			handleMessage(readBytes, client)
 
-		case <-client.Closed:
-			log.Println("Connection on client ", client.Name, " was closed")
-			return
+		case disconnectedClient := <-clientsMap.ClientUnregistered:
+
+			// notify everyone that a user has disconnected
+			var outboundChatMsg = goWebChat.NewServerClientPartMessage(disconnectedClient.Name)
+			outboundRaw, err := json.Marshal(outboundChatMsg)
+
+			if err != nil {
+				log.Println("Error marshaling message to JSON", outboundChatMsg)
+				return
+			}
+
+			clientsMap.BroadcastToAll <- outboundRaw
+
+		case connectedClient := <-clientsMap.ClientRegistered:
+			// notify everyone that a new user has connected
+			var outboundChatMsg = goWebChat.NewServerClientJoinMessage(connectedClient.Name)
+			outboundRaw, err := json.Marshal(outboundChatMsg)
+
+			if err != nil {
+				log.Println("Error marshaling message to JSON", outboundChatMsg)
+				return
+			}
+
+			clientsMap.BroadcastToAllExcept <- goWebChat.BroadcastToAllExcept{Name: connectedClient.Name, Content: outboundRaw}
+
+			retChan := make(chan []*goWebChat.Client)
+
+			clientsMap.GetAllClients <- retChan
+
+			// send the list of users to the newly connected user
+			go func() {
+				var users string
+
+				select {
+				case clients := <-retChan:
+
+					for _, k := range clients {
+						users = users + "," + k.Name
+					}
+				}
+
+				var outboundClientListMsg = goWebChat.NewServerClientListMessage(users)
+				outboundRaw, err = json.Marshal(outboundClientListMsg)
+
+				if err != nil {
+					log.Println("Error marshaling message to JSON", outboundChatMsg)
+					return
+				}
+
+				connectedClient.WriteChan <- outboundRaw
+			}()
+
 		}
 	}
 }
