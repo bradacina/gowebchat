b35337b3130b48038ec4025b9888a8ca424de3c3
Bogdan Radacina <bradacina@users.noreply.github.com>
2014-02-28 08:48:47 +1100

    Refactoring location of packages. Added proper detection of client ip if a proxy (nginx) is used

--- goWebChatServer/messageHandler.go
+++ goWebChatServer/messageHandler.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"code.metaconstudios.com/gowebchat/goWebChat"
 	"encoding/json"
 	"fmt"
-	"goWebChat"
 	"html"
 	"log"
 	"strings"
--- goWebChatServer/sendMessageHelper.go
+++ goWebChatServer/sendMessageHelper.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"code.metaconstudios.com/gowebchat/goWebChat"
 	"encoding/json"
-	"goWebChat"
 	"log"
 )
 
